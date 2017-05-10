25d0761553b46cfa1de19b13b22bf1dc0d661dde
Aaron Martin <aaron@eri.ucsb.edu>
2017-05-10 11:28:56 -0700

    Add actual jekyll build functionality into script, testing only at this point.

--- unix/webhookListener.sh
+++ unix/webhookListener.sh
@@ -9,3 +9,11 @@ echo "# caller: $0" >> $log
 echo "# args: $@" >> $log
 echo "# datestamp: $ds" >> $log
 echo "# user: $USER" >> $log
+
+update_waves_website(){
+    cd /home/caylor-lab/waves_website
+    jekyll build >> $log
+}
+
+# Ultimately this needs to be called only on the appropriate events, but for testing we will do this.
+update_waves_website
