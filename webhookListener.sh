92cf9d52c03f5be199512b453ab29a190b71dbfa
Aaron Martin <aaron@eri.ucsb.edu>
2017-05-10 16:29:52 -0700

    more refinements to shell script.

--- webhookListener.sh
+++ webhookListener.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # need to make sure that the user running the webhookListener.rb script can run this script and write to the log
 #
-log=/var/log/webhookListener.log
+log=WH_SCRIPTLOG
 
 ds=`date +'%Y%m%d-%H%M%S'`
 echo "######################################" >> $log
@@ -10,16 +10,27 @@ echo "# args: $@" >> $log
 echo "# datestamp: $ds" >> $log
 echo "# user: $USER" >> $log
 
+[ $# -lt 2 ] && { echo "# Warning: script called without enough args" >> $log; exit 2 ; }
+
+repo=$1; shift
+event=$1; shift
+
 update_waves_website(){
     lockfile=/tmp/webhookListener-waves-website-lock
-    [ -f $lockfile ] && { echo "# WARNING: Lockfile $lockfile exists, possibly a second webhook event before the first finished???" >> $log ; return 1; }
+    # want to avoid overlapping jekyll builds, so a lockfile is a real good idea
+    [ -f $lockfile ] && { echo "# WARNING: Lockfile $lockfile exists, possibly a second webhook event before the first f
+inished???" >> $log ; return 1; }
     touch $lockfile
     echo "cd to /home/caylor-lab/waves_website" >> $log
     cd /home/caylor-lab/waves_website
-    echo "Initiating jekyll build" >> $log
-    # The following can take a long time, but we need the overall webhook process to finish in a reasonable amount of time
-    (~/bin/jekyll build >> $log 2>&1 ; rm $lockfile; ) &
+    echo "Initiating git pull and jekyll build" >> $log
+    # The jekyll build can take a long time, but we need the overall webhook process
+    # to complete in a reasonable amount of time, so a backgrounded subshell should do the trick
+    (git pull >> $log 2>&1 ; ~/bin/jekyll build >> $log 2>&1 ; rm $lockfile; ) &
 }
 
 # Ultimately this needs to be called only on the appropriate events, but for testing we will do this.
-update_waves_website
+case $repo in
+    waves_sebsite)    update_waves_website ;;
+    *)                echo "# Not configured to handle events for repo: $repo" >> $log  ;;
+esac
